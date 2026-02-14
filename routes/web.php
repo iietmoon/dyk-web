0926c82186eb5c15806f5b25387288fbee439809
iietmoon <otmannj56@gmail.com>
2026-02-14 13:51:36 +0100

    feat: minor change in web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,3 @@ use App\Http\Controllers\Webhooks\PaddleController;
 
 include __DIR__ . '/website.php';
 include __DIR__ . '/webhooks.php';
-
-Route::post('webhooks/paddle/subscription-created', [PaddleController::class, 'subscriptionCreated'])
-    ->name('webhooks.paddle.subscription-created');
